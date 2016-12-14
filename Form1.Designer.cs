41443df40fbbcb17cdb24768516ef89a8d9d01e7
Nirav Pardiwala <niravpardiwala38@gmail.com>
2016-12-13 20:30:26 -0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace NiravProject1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textY = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textX = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbCalcType = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textResults = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(12, 46);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(553, 642);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(638, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "No of Rows";
+            // 
+            // textY
+            // 
+            this.textY.Location = new System.Drawing.Point(642, 81);
+            this.textY.Name = "textY";
+            this.textY.Size = new System.Drawing.Size(100, 26);
+            this.textY.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(765, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "No of Colums";
+            // 
+            // textX
+            // 
+            this.textX.Location = new System.Drawing.Point(769, 81);
+            this.textX.Name = "textX";
+            this.textX.Size = new System.Drawing.Size(100, 26);
+            this.textX.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(896, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Calculation Type";
+            // 
+            // cmbCalcType
+            // 
+            this.cmbCalcType.FormattingEnabled = true;
+            this.cmbCalcType.Items.AddRange(new object[] {
+            "Add",
+            "Subtract",
+            "Multiply",
+            "Divide"});
+            this.cmbCalcType.Location = new System.Drawing.Point(900, 78);
+            this.cmbCalcType.Name = "cmbCalcType";
+            this.cmbCalcType.Size = new System.Drawing.Size(121, 28);
+            this.cmbCalcType.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1049, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 49);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textResults
+            // 
+            this.textResults.Location = new System.Drawing.Point(642, 133);
+            this.textResults.Multiline = true;
+            this.textResults.Name = "textResults";
+            this.textResults.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textResults.Size = new System.Drawing.Size(533, 642);
+            this.textResults.TabIndex = 8;
+            this.textResults.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1243, 700);
+            this.Controls.Add(this.textResults);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cmbCalcType);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textX);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textY);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textY;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textX;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbCalcType;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textResults;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NiravProject1
+{
+    public partial class Form1 : Form
+    {
+        public enum CalculationType : byte
+        {
+            Add = 1,
+            Subtract = 2,
+            Multiply = 3,
+            Divide = 4
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            cmbCalcType.DataSource = Enum.GetValues(typeof(CalculationType));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            segmented_identifier();
+
+        }
+
+        private void segmented_identifier()
+        {
+            int value1 = 65;
+            int value2 = 1;
+            int value3 = 119;
+            int value4 = 2;
+            string str2 = " ";
+            string str;
+            for (value1 = 65; value1 < 70; value1++)
+            {
+                for (value2 = 1; value2 < 7; value2 += 2)
+                {
+                    for (value3 = 119; value3 < 123; value3++)
+                    {
+                        for (value4 = 2; value4 < 12; value4 += 2)
+                        {
+                            char seg1 = (char)value1;
+                            char seg3 = (char)value3;
+                            str = string.Format("{0}{1}{2}{3} ", seg1, value2, seg3, value4);
+                            string str3 = str2 + str;
+                            textBox1.Text = (str2 += str + "\t \t");
+                            str2 = str3;
+
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textResults.Text = String.Empty;
+            Int16 X = ValidateStringAsNumericX(textX);
+            Int16 Y = ValidateStringAsNumericY(textY);
+            CalculationType CalcType;
+            Enum.TryParse<CalculationType>(cmbCalcType.SelectedItem.ToString(), out CalcType);
+            String[,] CalculatedArray = GetCalculatedArray(X, Y, CalcType);
+            DisplayCalculatedArray(CalculatedArray);
+        }
+
+        private Int16 ValidateStringAsNumericY(TextBox textY)
+        {
+            try
+            {
+                Int16 TextY = Convert.ToInt16(textY.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Invalid Input for No of rows.", "Entry Error");
+                textY.Focus();
+                textX.Clear();
+            }
+            return Int16.Parse(textX.Text);
+        }
+        private Int16 ValidateStringAsNumericX(TextBox textX)
+        {
+            try
+            {
+                Int16 TextX = Convert.ToInt16(textX.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Invalid Input for No of Columns.", "Entry Error");
+                textX.Focus();
+                textX.Clear();
+            }
+            return Int16.Parse(textX.Text);
+        }       
+        private String[,] GetCalculatedArray(Int16 XAxis, Int16 YAxis, CalculationType calcType)
+        {
+            int Row = int.Parse(textY.Text);
+            int Column = int.Parse(textX.Text);
+            String[,] rectangularArray = new String[Row, Column];
+            for (int i = 0; i < Row; i++)
+            {
+                for (int j = 0; j < Column; j++)
+                {
+                    switch (calcType)
+                    {
+                        case CalculationType.Add:
+                            rectangularArray[i, j] = (i + j).ToString();
+                            break;
+                        case CalculationType.Subtract:
+                            rectangularArray[i, j] = (i - j).ToString();
+                            break;
+                        case CalculationType.Multiply:
+                            rectangularArray[i, j] = (i * j).ToString();
+                            break;
+                        case CalculationType.Divide:
+                            if (j == 0)
+                            {
+                                rectangularArray[i, j] = "N/A";
+                            }
+                            else
+                            {
+                                rectangularArray[i, j] = (i / j).ToString();
+                            }
+                            break;
+                    }
+                }
+            }
+            return rectangularArray;
+        }
+        private void DisplayCalculatedArray(String[,] rectangularArray)
+        {            
+            CalculationType CalcType;
+            Enum.TryParse<CalculationType>(cmbCalcType.SelectedValue.ToString(), out CalcType);
+            int Row = int.Parse(textY.Text);
+            int Column = int.Parse(textX.Text);
+            textResults.Text = "";
+            for(int x = 0; x < Row; x++)
+            {               
+                for (int y = 0; y < Column; y ++)
+                {
+                    if (CalcType == CalculationType.Add)
+                    {
+                        textResults.Text += x + " + " + y + " = " + rectangularArray[x, y] + "\t \t";
+                    }
+                    else if (CalcType == CalculationType.Subtract)
+                    {
+                        textResults.Text += x + " - " + y + " = " + rectangularArray[x, y] + "\t \t";
+                    }
+                    else if (CalcType == CalculationType.Multiply)
+                    {
+                        textResults.Text += x + " * " + y + " = " + rectangularArray[x, y] + "\t \t";
+                    }
+                    else
+                    {
+                        textResults.Text += x + " / " + y + " = " + rectangularArray[x, y] + "\t \t";
+                    }
+                }
+                textResults.Text += Environment.NewLine;
+
+            }
+        }
+    }
+}
